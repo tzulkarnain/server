3c5a000c70e5d2a1d5bcd4bfd81e7984e927fc50
Timothy Zulkarnain <TZ@Timothys-MacBook-Air.local>
2017-11-03 15:30:38 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/endpointName', function (req, res) {
+    //res.header("Access-Control-Allow-Origin", "*");
+    res.send(JSON.stringify("hello"))
+});
+
+app.listen(4000, function () {
+    console.log('Example app listening on port 4000!')
+})
