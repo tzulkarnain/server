5f10d60c03e740dddb4d1f04b9c3cf61287a2d64
Timothy Zulkarnain <TZ@Timothys-MacBook-Air.local>
2017-11-03 16:03:05 -0400

    update

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 
 app.get('/endpointName', function (req, res) {
-    //res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*");
     res.send(JSON.stringify("hello"))
 });
 
-app.listen(4000, function () {
+app.listen(process.env.PORT || 4000, function () {
     console.log('Example app listening on port 4000!')
 })
